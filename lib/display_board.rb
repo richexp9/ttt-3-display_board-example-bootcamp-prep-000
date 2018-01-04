312f28e095a7a758d7e7c3ed9d9b847c0735b89d
Richard Loera <richexp9@gmx.com>
2018-01-04 13:26:23 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,4 +7,39 @@
 #   print "   |   |   \n"
 # end
 
+
+Nolan Neeley 14 MINUTES AGO
+Here's my code:
+
+Nolan Neeley 14 MINUTES AGO
+# Define a method display_board that prints a 3x3 Tic Tac Toe Board def display_board print " " + "|" + " " + "|" + " " print "-----------" print " " + "|" + " " + "|" + " " print "-----------" print " " + "|" + " " + "|" + " " end display_board
+
+Joshua Owens 10 MINUTES AGO
+Good morning. I will be with you momentarily.
+
+Joshua Owens 10 MINUTES AGO
+In the meantime can you share your code and error in a snippet
+
+Joshua Owens 10 MINUTES AGO
+You can start a snippet by typing 3 backticks (the thing under the ~, to the left of the 1) in an empty chat.  A new window-ish thing should open up.
+
+It
+
+              preserves
+
+                           formatting.
+
+Also this:
+http://help.learn.co/ask-a-question/how-do-i-paste-code-in-a-question
+
+Nolan Neeley 6 MINUTES AGO
+# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+print "   " + "|" + "   " +  "|" + "  "
+print "-----------"
+print "   " + "|" + "   " +  "|" + "  "
+print "-----------"
+print "   " + "|" + "   " +  "|" + "  "
+end
+
 display_board
